ef1f3e43ffbd69c7b7f996578ace46ae87816a0f
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-06-07 16:28:51 +0100

    lfw212 created lab 3.1 node express

---
+++ labs/lab_3_1_deliver_data_from_library_api/node_express/app.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const dataLib = require('./data');
+
+const PORT = 3000;
+const HOST = 'localhost';
+const URI = `http://${HOST}:${PORT}`;
+
+const app = express();
+
+app.get('/', async (req, res) => {
+    const result = await dataLib();
+    res.setHeader('Content-Type', 'application/json');
+    return res.status(200).send(result);
+});
+
+app.get('*', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    return res.status(404).send(JSON.stringify({ error: 'Not found' }));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at ${URI}`);
+});
