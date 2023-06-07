22266d4a287e37f515d366d32f21d320919ce875
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-06-07 16:28:18 +0100

    lfw212 created lab 3.1 node http core

---
+++ labs/lab_3_1_deliver_data_from_library_api/node_http/README.md
@@ -0,0 +1,13 @@
+# NodeJS_Services_Development_LFW212
+
+## Lab
+
+### Lab 3.1 Deliver Data from a Library API
+
+#### Install:
+No installation needed, since it is using http node core.
+
+#### Validate:
+```
+node validate
+```
---
+++ labs/lab_3_1_deliver_data_from_library_api/node_http/app.js
@@ -0,0 +1,24 @@
+
+const http = require('node:http');
+const dataLib = require('./data');
+
+const PORT = 3000;
+const HOST = 'localhost';
+const URI = `http://${HOST}:${PORT}`;
+
+const server = http.createServer(async (req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    const result = await dataLib();
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(result);
+    return res.end();
+  } else {
+    res.writeHead(404, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify({error: 'Not found'}));
+    return res.end();
+  }
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running at ${URI}`);
+});
