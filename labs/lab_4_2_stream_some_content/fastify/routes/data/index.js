7b6ca8ea3c9e16ef8c33dd112f9932528b738b30
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-06-08 22:10:04 +0100

    lfw212 fastify add lab 4.2 stream some content

---
+++ labs/lab_4_2_stream_some_content/fastify/routes/data/index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const { Readable, Transform } = require('stream');
+
+const stream = () => {
+    const readable = Readable.from(['this', 'is', 'a', 'stream', 'of', 'data'].map((s) => s + '<br>'))
+    const delay = new Transform(({transform (chunk, enc, cb) {setTimeout(cb, 500, null, chunk)}}))
+    return readable.pipe(delay)
+}
+
+module.exports = async (fastify, opts) => {
+  fastify.get('/', async (request, reply) => {
+    return stream()
+  })
+}
