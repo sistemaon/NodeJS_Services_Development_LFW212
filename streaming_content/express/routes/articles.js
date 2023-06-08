2eafb471e54d1277f24573c0ae3bbe39be272737
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-06-08 21:05:59 +0100

    lfw212 express add streaming content.

---
+++ streaming_content/express/routes/articles.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const hnLatestStream = require('hn-latest-stream')
+const finished = require('stream').finished
+
+router.get('/', function(req, res, next) {
+  const { amount = 10, type = 'html' } = req.query
+
+  if (type === 'html') res.type('text/html')
+  if (type === 'json') res.type('application/json')
+
+  const stream = hnLatestStream(amount, type)
+
+  stream.pipe(res, {end: false})
+
+  finished(stream, (err) => {
+    if (err) {
+      next(err)
+      return
+    }
+    res.end()
+  })
+
+});
+
+module.exports = router;
