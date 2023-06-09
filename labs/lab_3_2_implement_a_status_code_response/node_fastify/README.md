e28242118e33031f45579e99d88d7561b128a0fa
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-06-09 14:30:45 +0100

    lfw212 node fastify add lab 3.2 implement a status code response

---
+++ labs/lab_3_2_implement_a_status_code_response/node_fastify/README.md
@@ -0,0 +1,14 @@
+# NodeJS_Services_Development_LFW212
+
+## Lab
+
+### Lab 3.2 Implement a Status Code Response
+
+#### Install:
+```
+npm install
+```
+#### Validate:
+```
+node validate
+```
---
+++ labs/lab_3_2_implement_a_status_code_response/node_fastify/app.js
@@ -0,0 +1,35 @@
+
+const fastify = require('fastify')({ logger: true });
+const dataLib = require('./data');
+
+const PORT = 3000;
+const HOST = 'localhost';
+const URI = `http://${HOST}:${PORT}`;
+
+fastify.get('/', async (req, res) => {
+    const result = await dataLib();
+    res.header('Content-Type', 'application/json');
+    return res.status(200).send(result);
+});
+
+fastify.post('/', async (req, res) => {
+    res.header('Content-Type', 'application/json');
+    return res.status(405).send(JSON.stringify({ error: 'Method Not Allowed HTTP Status' }));
+});
+
+fastify.get('*', (req, res) => {
+    res.header('Content-Type', 'application/json');
+    return res.status(404).send(JSON.stringify({ error: 'Not found' }));
+});
+
+const start = async () => {
+    try {
+        await fastify.listen({ port: PORT })
+        console.log(`Server running at ${URI}`);
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+}
+
+start();
