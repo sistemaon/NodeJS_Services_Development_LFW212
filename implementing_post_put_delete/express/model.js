e90ab9f57eeb096ef5e71688c95e4631ce576e81
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-06-12 14:08:18 +0100

    lfw212 node express add exercise implementing post put delete explain

--- implementing_post_put_delete/express/model.js
+++ implementing_post_put_delete/express/model.js
@@ -1,12 +1,38 @@
 'use strict'
 
-const bicycleModel = () => {
+module.exports = {
+    bicycle: bicycleModel()
+}
+
+function bicycleModel() {
     const db = {
         1: { brand: 'Veloretti', color: 'green' },
         2: { brand: 'Batavus', color: 'yellow' }
     }
-    
-    const read = (id, cb) => {
+
+    return {
+        create, read, update, del, uid
+    }
+
+    function uid() {
+        return Object.keys(db)
+            .sort((a, b) => a - b)
+            .map(Number)
+            .filter((n) => !isNaN(n))
+            .pop() + 1 + ''
+    }
+
+    function create(id, data, cb) {
+        if (db.hasOwnProperty(id)) {
+            const err = Error('resource exists')
+            setImmediate(() => cb(err))
+            return
+        }
+        db[id] = data
+        setImmediate(() => cb(null, id))
+    }
+
+    function read(id, cb) {
         if (!(db.hasOwnProperty(id))) {
             const err = Error('not found')
             setImmediate(() => cb(err))
@@ -15,11 +41,24 @@ const bicycleModel = () => {
         setImmediate(() => cb(null, db[id]))
     }
 
-    return {
-        read
+    function update(id, data, cb) {
+        if (!(db.hasOwnProperty(id))) {
+            const err = Error('not found')
+            setImmediate(() => cb(err))
+            return
+        }
+        db[id] = data
+        setImmediate(() => cb())
     }
-}
 
-module.exports = {
-    bicycle: bicycleModel()
+    function del(id, cb) {
+        if (!(db.hasOwnProperty(id))) {
+            const err = Error('not found')
+            setImmediate(() => cb(err))
+            return
+        }
+        delete db[id]
+        setImmediate(() => cb())
+    }
+
 }
