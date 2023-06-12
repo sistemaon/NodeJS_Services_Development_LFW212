b79658dfd320bd2ff04374a348098b1f21393a1c
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-06-12 17:06:26 +0100

    lfw212 node fastify add lab 6.1 implementing a restful json post

--- labs/lab_6_1_implement_a_restful_json_post/fastify/routes/boat/index.js
+++ labs/lab_6_1_implement_a_restful_json_post/fastify/routes/boat/index.js
@@ -3,15 +3,28 @@
 const { promisify } = require('util')
 const { boat } = require('../../model')
 const read = promisify(boat.read)
+const create = promisify(boat.create)
 
 module.exports = async (fastify, opts) => {
   const { notFound } = fastify.httpErrors
+
   fastify.get('/:id', async (request, reply) => {
     const { id } = request.params
     try {
       return await read(id)
     } catch (err) {
       if (err.message === 'not found') throw notFound()
+      throw err
+    }
+  })
+
+  fastify.post('/', async (request, reply) => {
+    const id = boat.uid()
+    try {
+      const result = await create(id, request.body.data)
+      reply.code(201)
+      return { id: result }
+    } catch (err) {
       throw err
     }
   })
