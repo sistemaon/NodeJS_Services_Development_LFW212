8cb667c0ffa0ca59f068bfa31d478eff51043de4
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-06-14 14:59:02 +0100

    lfw212 node add fastify Proxying HTTP Requests part single origin multi route proxy

---
+++ proxying_http_requests/single_origin_multi_route_proxy/fastify/app.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const path = require('path')
+const AutoLoad = require('@fastify/autoload')
+
+const proxy = require('@fastify/http-proxy')
+
+// Pass --options via CLI arguments in command to enable these options.
+module.exports.options = {}
+
+module.exports = async function (fastify, opts) {
+  // Place here your custom code!
+
+  fastify.register(proxy, {
+    upstream: 'https://news.ycombinator.com',
+    async preHandler(request, reply) {
+      if (request.query.token !== 'abc') {
+        throw fastify.httpErrors.unauthorized()
+      }
+    }
+  })
+
+  // Do not touch the following lines
+
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+    options: Object.assign({}, opts)
+  })
+
+  // This loads all plugins defined in routes
+  // define your routes in one of these
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'routes'),
+    options: Object.assign({}, opts)
+  })
+}
