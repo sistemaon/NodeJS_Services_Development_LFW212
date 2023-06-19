54ff29732c229bc42627f4575c349dbeceec0155
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-06-19 15:11:09 +0100

    lfw212 node express web security user input fix

--- web_security_handling_user_input/express/app.js
+++ web_security_handling_user_input/express/app.js
@@ -1,7 +1,6 @@
 
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -11,15 +10,10 @@ const bicycleRouter = require('./routes/bicycle');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
