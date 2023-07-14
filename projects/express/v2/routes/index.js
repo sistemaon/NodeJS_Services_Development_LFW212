34e7fe2eba26e04a89ecff78e31e19d4cb85db25
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-07-14 18:57:28 +0100

    lfw212 add project t2 v2 view route hello world

--- projects/express/v2/routes/index.js
+++ projects/express/v2/routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/hwdummy00', function(req, res, next) {
+  const { name } = req.query;
+  if (!name) {
+    return res.status(400).send('Missing name query parameter');
+  }
+  return res.render('helloWorldDummy00', { name: name });
 });
 
 module.exports = router;
