641807c2bb38ca9c205d482f4c7447596e0cdba7
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-07-14 19:52:46 +0100

    lfw212 add project t2 v2 hello static

--- projects/express/v2/readme.md
+++ projects/express/v2/readme.md
@@ -1,7 +1,15 @@
 
 # Creating a Web Server
 
-**files**: _routes/index.js_, _views/helloWorldDummy00.hbs_ 
-**route path**: _/hwdummy00_
-**query**: _name_
-**example**: _/hwdummy00?name=SistemaOn_
+>**files**: _routes/index.js_, _views/helloWorldDummy00.hbs_\
+>**route path**: _/hwdummy00_\
+>**query**: _name_\
+>**example**: _/hwdummy00?name=SistemaOn_\
+
+>**files**: _routes/index.js_\
+>**route path**: _/hello/static_\
+>**params** > _pathName_\
+>**routes available**: _/hello/static/root_, _/hello/static/hello_\
+
+---
+--
--- projects/express/v2/routes/index.js
+++ projects/express/v2/routes/index.js
@@ -1,19 +1,77 @@
 
 const express = require('express');
 const router = express.Router();
+const { URL } = require('url');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 /* GET home page. */
-router.get('/hwdummy00', function(req, res, next) {
+router.get('/hwdummy00', function (req, res, next) {
   const { name } = req.query;
   if (!name) {
     return res.status(400).send('Missing name query parameter');
   }
   return res.render('helloWorldDummy00', { name: name });
 });
+
+
+router.get('/hello/static/:pathName', (req, res, next) => {
+  res.setHeader('Content-Type', 'text/html');
+  if (req.method !== 'GET') {
+    return res.status(405).send('Method in this route not allowed!');
+  };
+
+  const pathNames = [ 'hello', 'root' ];
+  const { pathName } = req.params;
+  if (!pathNames.includes(pathName)) {
+    return res.status(404).send('Path Not Found!');
+  }
+
+  const root = `
+    <html>
+      <head>
+        <style>
+        body { background: #333; margin: 1.25rem }
+        a { color: yellow; font-size: 2rem; font-family: sans-serif }
+        </style>
+      </head>
+      <body>
+        <a href='/hello/static/hello'>Hello</a>
+      </body>
+    </html>
+  `;
+  const hello = `
+    <html>
+      <head>
+        <style>
+        body { background: #333; margin: 1.25rem }
+        h1 { color: #EEE; font-family: sans-serif }
+      </style>
+      </head>
+      <body>
+        <h1>Hello World</h1>
+      </body>
+    </html>
+  `;
+
+  const protocol = req.protocol;
+  const host = req.headers.host;
+  const path = req.path;
+  const urlPath = `${protocol}://${host}${path}`;
+  const url = new URL(urlPath);
+
+  if (pathName === 'root' && url.pathname === '/hello/static/root') {
+    return res.end(root);
+  }
+  if (pathName === 'hello' && url.pathname === '/hello/static/hello') {
+    return res.end(hello);
+  }
+
+  return res.status(404).send('Not Found!');
+});
+
 
 module.exports = router;
