568a21e798a458d199efeaaef3d512e629db9e5e
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-07-21 19:05:43 +0100

    lfw212 route model 5.1 update

--- projects/express/v2/routes/lab.5.1.js
+++ projects/express/v2/routes/lab.5.1.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const model = require('../models/lab.5.1.model');
 
-router.get('/:id', function (req, res, next) {
+router.get('/:id', (req, res, next) => {
     model.boat.read(req.params.id, (err, result) => {
         if (err) {
             err && err.message === 'not found' ? next() : next(err);
