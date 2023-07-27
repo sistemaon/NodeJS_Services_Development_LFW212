a427e62d9de53fb1ef4b67b63b37e7cb6df6b0d3
lenovo_ideapad_gaming_3 <erni.augusto@gmail.com>
2023-07-27 21:01:01 +0100

    lfw212 project express v3 restfull delete get

---
+++ projects/express/v3/l.6.2/index.js
@@ -0,0 +1,52 @@
+
+const express = require('express');
+const router = express.Router();
+const model = require('./model');
+
+router.get('/:id', (req, res, next) => {
+    res.setHeader('content-type', 'application/json');
+
+    model.boat.read(req.params.id, (err, result) => {
+        if (err) {
+            err && err.message === 'not found' ? next() : next(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    try {
+        const id = model.boat.uid();
+        const { brand, color } = req.body.data;
+
+        model.boat.create(id, { brand, color }, (err, result) => {
+            if(err) {
+                return err.message === 'resource exists' ? next() : next(err);
+            }
+            return res.status(201).send({ id: id });
+        });
+        
+    } catch (error) {
+        return next(error);
+    }
+});
+
+router.delete('/:id', (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    try {
+        const { id } = req.params;
+        model.boat.del(id, (err, result) => {
+            if (err) {
+                return err.message === 'not found' ? next() : next(err);
+            }
+            return res.status(204).send();
+        });      
+    } catch (error) {
+        return next(error);
+    }
+
+});
+
+module.exports = router;
